6afb69b2df41b9092fc897bc99aa77768b807e87
dy317 <kyl123451@naver.com>
2025-06-04 13:46:08 +0900

    [feature] 39p

--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -31,6 +31,6 @@ class GameTest {
         assertIllegalArgument(null);
         assertIllegalArgument("12");
         assertIllegalArgument("1234");
-        assertIllegalArgument("12s");
+        assertIllegalArgument("121");
     }
 }
