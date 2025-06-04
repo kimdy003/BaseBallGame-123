37327c7d4a439b6bbf0cf2680137a74aff65e559
dy317 <kyl123451@naver.com>
2025-06-04 13:38:11 +0900

    [feature] 28p

--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -23,4 +23,11 @@ class GameTest {
             game.guess(null);
         });
     }
+
+    @Test
+    void throwExceptionWhenInputLengthIsUnmatched() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            game.guess("12");
+        });
+    }
 }
