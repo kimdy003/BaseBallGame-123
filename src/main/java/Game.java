3a34e72b03acae34a701deea08853aa6b2304802
dy317 <kyl123451@naver.com>
2025-06-04 13:46:47 +0900

    [refactoring] 40p

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -14,10 +14,14 @@ public class Game {
             }
         }
 
-        if (guessNumber.charAt(0) == guessNumber.charAt(1)
-                || guessNumber.charAt(0) == guessNumber.charAt(2)
-                || guessNumber.charAt(1) == guessNumber.charAt(2)) {
+        if (isDuplicatedNumber(guessNumber)) {
             throw new IllegalArgumentException();
         }
+    }
+
+    private static boolean isDuplicatedNumber(String guessNumber) {
+        return guessNumber.charAt(0) == guessNumber.charAt(1)
+                || guessNumber.charAt(0) == guessNumber.charAt(2)
+                || guessNumber.charAt(1) == guessNumber.charAt(2);
     }
 }
