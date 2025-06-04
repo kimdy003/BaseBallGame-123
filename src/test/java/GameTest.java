ef7f65369d204ab9330a9837bf71be31285872b3
dy317 <kyl123451@naver.com>
2025-06-04 13:44:30 +0900

    [feature] 36p

--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -17,13 +17,6 @@ class GameTest {
         assertNotNull(game);
     }
 
-    @Test
-    void throwIllegalArgumentExceptionInvalidInput() {
-        assertIllegalArgument(null);
-        assertIllegalArgument("12");
-        assertIllegalArgument("1234");
-    }
-
     private void assertIllegalArgument(String guessNumber) {
         try {
             game.guess(guessNumber);
@@ -31,5 +24,13 @@ class GameTest {
         } catch (IllegalArgumentException e) {
 
         }
+    }
+
+    @Test
+    void throwIllegalArgumentExceptionInvalidInput() {
+        assertIllegalArgument(null);
+        assertIllegalArgument("12");
+        assertIllegalArgument("1234");
+        assertIllegalArgument("12s");
     }
 }
